9ea40177ae6ce1b3fb53a3ec62f4bd63ad3bb1d0
tenzinrabjam2024-ux <tenzinrabjam2024@gmail.com>
2025-10-29 18:28:28 -0600

    initial commit - program 1 updated

---
+++ Program1.py
@@ -0,0 +1,24 @@
+# Name: Tenzin
+# Assignment: Project 1 (+2)
+
+# \ = Escape Character in coding
+# \n = new line
+
+ # Step1
+# print("  @..@ ")
+# print(" (----)")
+# print("( >__< )")
+# print("^^ ~~ ^^")
+
+first = "  @..@ "
+second = " (----) "
+third = "( >__< )"
+fourth = "^^ ~~ ^^"
+print(first + "\n" + second + "\n" + third + "\n" + fourth)
+
+# print("  @..@   \n (----) \n( >__< )\n^^ ~~ ^^")
+
+# Step2
+state = input("US State: ")
+capital = input("Capital: ")
+print("The capital of " + state + " is " + capital)
